acbe29ff65d7f12c3951cf4cb900fc183c773e30
Jonah Pool <jpool.school@gmail.com>
2021-03-05 17:00:34 -0500

    Add object querying

--- public/script.js
+++ public/script.js
@@ -1,6 +1,25 @@
-async function windowActions {
-    endpoint = 
-    const rests = [];
-    const request
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const restaurants = [];
+  
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => restaurants.push(...data));
+    
+  //console.log(restaurants);
+  //console.log(restaurants);
+
+function findMatches(wordToMatch, restaurants) {
+  return restaurants.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) // change these values based on what filters we decide on.
+  });
 }
-window.onload = windowActions;
+
+function displayMatches() {
+  console.log(this.value);
+}
+
+const searchInput  = document.querySelector('.search-bar');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
