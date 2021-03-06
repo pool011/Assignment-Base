731b4409875db56899b253a3a415da6585113361
Jonah Pool <jpool.school@gmail.com>
2021-03-05 23:01:39 -0500

    fix indent

--- public/script.js
+++ public/script.js
@@ -1,64 +1,56 @@
 async function windowActions() {
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-const request = await fetch(endpoint);
-const restaurants = await request.json();
+  const request = await fetch(endpoint);
+  const restaurants = await request.json();
 
-function findMatches(wordToMatch, restaurants) {
-  const tempArr = restaurants.filter(place => {
-    const regex = new RegExp(wordToMatch, 'gi');
-    return place.city.match(regex) || place.name.match(regex) || place.category.match(regex)
-  });
-  const tempSet = new Set(tempArr.map(place => place.establishment_id));
-  const unique_ids = Array.from(tempSet).slice(0,9);
-  console.log(unique_ids);
-  let id_length  = unique_ids.length - 1;
-  const results = [];
-  for (place in restaurants) {
-    if (restaurants[place].establishment_id == unique_ids[id_length]) {
-      //console.log(id);
-      //console.log(unique_ids.slice(id_length-10,id_length));
-      console.log(unique_ids.pop());
-      id_length -= 1;
-      //console.log(place);
-      let catThis = restaurants[place];
-      results.push(catThis);
+  function findMatches(wordToMatch, restaurants) {
+    const tempArr = restaurants.filter(place => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.city.match(regex) || place.name.match(regex) || place.category.match(regex)
+    });
+    const tempSet = new Set(tempArr.map(place => place.establishment_id));
+    const unique_ids = Array.from(tempSet).slice(0,9);
+    console.log(unique_ids);
+    let id_length  = unique_ids.length - 1;
+    const results = [];
+    for (place in restaurants) {
+      if (restaurants[place].establishment_id == unique_ids[id_length]) {
+        //console.log(id);
+        //console.log(unique_ids.slice(id_length-10,id_length));
+        console.log(unique_ids.pop());
+        id_length -= 1;
+        //console.log(place);
+        let catThis = restaurants[place];
+        results.push(catThis);
+      }
     }
+    return results;//.slice(0,9);
   }
-  return results;//.slice(0,9);
-}
 
-function displayMatches(event) {
-  const matchArray = findMatches(event.target.value, restaurants);
-  const html = matchArray.map(place => {
-    return `
-    <div class="box">
-      <li>
-<<<<<<< HEAD
-        <span class="name">${place.name} ${place.establishment_id}</span> <br>
-        <span class="address">${place.address_line_1} <br>
-        ${place.city}, ${place.state} ${place.zip}</span> <br>
-        <span class="category">Category: ${place.category} <br>
-        <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</br></span>
-=======
-        <span class="name">${place.name}</span> <br>
-        <span class="category">${place.category}</span> <br>
-        <span class="address">${place.address_line_1}</span> <br>
-        ${place.city}, ${place.state} ${place.zip}</span> <br>
-        <span class="category">${place.category} <br>
-        <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</span>
->>>>>>> b123dc4fa7baa6f81f885c8eb0536934bae2dcbb
-      </li>
-    </div>
-    `
-  }).join('');
-  suggestions.innerHTML= html;
-}
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, restaurants);
+    const html = matchArray.map(place => {
+      return `
+      <div class="box">
+        <li>
+          <span class="name">${place.name}</span> <br>
+          <span class="category">${place.category}</span> <br>
+          <span class="address">${place.address_line_1}</span> <br>
+          ${place.city}, ${place.state} ${place.zip}</span> <br>
+          <span class="category">${place.category} <br>
+          <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</span>
+        </li>
+      </div>
+      `
+    }).join('');
+    suggestions.innerHTML= html;
+  }
 
-const searchInput  = document.querySelector('.input');
-const suggestions = document.querySelector('.suggestions');
+  const searchInput  = document.querySelector('.input');
+  const suggestions = document.querySelector('.suggestions');
 
-searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
 
 }
 window.onload = windowActions();
