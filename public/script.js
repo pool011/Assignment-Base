7be2b9f6070ff23ea8af82fc76528e09a10365a3
Jonah Pool <jpool.school@gmail.com>
2021-03-05 22:55:30 -0500

    Dup attempts

--- public/script.js
+++ public/script.js
@@ -9,10 +9,27 @@ fetch(endpoint)
   //console.log(restaurants);
 
 function findMatches(wordToMatch, restaurants) {
-  return restaurants.filter(place => {
+  const tempArr = restaurants.filter(place => {
     const regex = new RegExp(wordToMatch, 'gi');
     return place.city.match(regex) || place.name.match(regex) || place.category.match(regex)
   });
+  const tempSet = new Set(tempArr.map(place => place.establishment_id));
+  const unique_ids = Array.from(tempSet).slice(0,9);
+  console.log(unique_ids);
+  let id_length  = unique_ids.length - 1;
+  const results = [];
+  for (place in restaurants) {
+    if (restaurants[place].establishment_id == unique_ids[id_length]) {
+      //console.log(id);
+      //console.log(unique_ids.slice(id_length-10,id_length));
+      console.log(unique_ids.pop());
+      id_length -= 1;
+      //console.log(place);
+      let catThis = restaurants[place];
+      results.push(catThis);
+    }
+  }
+  return results;//.slice(0,9);
 }
 
 function displayMatches() {
@@ -20,11 +37,11 @@ function displayMatches() {
   const html = matchArray.map(place => {
     return `
       <li>
-        <span class="name">${place.name}</span> <br>
+        <span class="name">${place.name} ${place.establishment_id}</span> <br>
         <span class="address">${place.address_line_1} <br>
         ${place.city}, ${place.state} ${place.zip}</span> <br>
         <span class="category">Category: ${place.category} <br>
-        <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</span>
+        <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</br></span>
       </li>
     `
   }).join('');
